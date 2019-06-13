f492a7015db57ada9deb6fb7d3d712f5192a5c46
alex sagel <alex.sagel7587@gmail.com>
2019-06-13 18:45:47 -0400

    added network image test

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// Flutter code sample for material.Scaffold.1
+
+// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
+// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
+// to center the text within the [Scaffold] and the [FloatingActionButton] is
+// centered and docked within the [BottomAppBar] using
+// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
+// connected to a callback that increments a counter.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://www.yosemite.com/wp-content/uploads/2019/03/places-to-stay-tablet-768x450.jpg')
+          ),
+        ),
+      ),
+    ));
